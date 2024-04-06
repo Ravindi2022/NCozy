fb5ef5f38278064a6aa5f389ab3830071e35356b
Ravindi Wickramasinghe <121094369+Ravindi2022@users.noreply.github.com>
2024-04-06 22:14:05 +0530

    Add files via upload

---
+++ adminNavbar.php
@@ -0,0 +1,52 @@
+<html>
+<head>
+<link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/0.7.5/flowbite.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+    <title>NSBM Accommodation Management</title>
+    
+</head>
+<body>
+
+ <nav class="bg-white border-gray-600 fixed w-full z-20 top-0 start-0 border-gray-600 bg-white mt-0 shadow-md">
+  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto py-2">
+    <a href="#" class="flex items-center space-x-2 rtl:space-x-reverse">
+        <img src="https://drive.google.com/file/d/1Nwpvd_IexWNX1CbBiQ50nFegZcVGubhQ/view?usp=sharing" class="h-10" alt="NCozy Logo" />
+        <span class="self-center text-2xl font-semibold whitespace-nowrap">NCozy</span>
+    </a>
+    <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 " aria-controls="navbar-default" aria-expanded="false">
+        <span class="sr-only">Open main menu</span>
+        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+        </svg>
+    </button>
+    <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+      <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white ">
+        <li>
+          <a href="../admin/home.php" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0" aria-current="page">Home</a>
+        </li>
+        <li>
+          <a href="../admin/addStudent.php" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Edit Student</a>
+        </li>
+        <li>
+          <a href="ViewEditLandlord.php" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 ">Edit Landlord</a>
+        </li>
+        <li>
+          <a href="ViewEditWarden.php" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 ">Edit Warden</a>
+        </li>
+        <li>
+          <a href="../admin/addArticle.php" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 ">Add Article</a>
+        </li>
+        <li>
+          <a href="../admin/addWarden.php" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 ">Edit Articles</a>
+        </li>
+        <li>
+          <a href="../admin/feedbackEdit.php" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 ">Edit Feedbacks</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>  
+</body>
+</html>
