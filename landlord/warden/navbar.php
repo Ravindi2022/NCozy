eae453f26c28d717f97f72d98eedcb5cf28cf293
Ravindi Wickramasinghe <121094369+Ravindi2022@users.noreply.github.com>
2024-05-08 22:03:18 +0530

    Add files via upload

---
+++ landlord/warden/navbar.php
@@ -0,0 +1,40 @@
+<nav class="bg-white border-gray-600 fixed w-full z-20 top-0 start-0 border-gray-600 bg-white mt-0 shadow-md">
+  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto py-2">
+    <a href="#" class="flex items-center space-x-2 rtl:space-x-reverse">
+        <img src="NCozy.png" class="h-10" alt="Flowbite Logo" />
+        <span class="self-center text-2xl font-semibold whitespace-nowrap">NCozy</span>
+    </a>
+    <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 " aria-controls="navbar-default" aria-expanded="false">
+        <span class="sr-only">Open main menu</span>
+        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+        </svg>
+    </button>
+    <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+      <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white ">
+      <li class="mt-1">
+          <a href="home.php" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0" aria-current="page">Home</a>
+        </li>
+        
+        <li class="mt-1">
+          <a href="articles.php" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 ">Articles</a>
+        </li>
+        <li class="mt-1">
+          <a href="../landlord/student/hostels.php" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 ">Hostels</a>
+        </li>
+        <li class="mt-1">
+          <a href="feedback.php" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 ">Feedbacks</a>
+        </li>
+        <li class="mt-1">
+          <a href="contact.php" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 ">Contact</a>
+        </li>
+        <li className="mr-0">
+          <a href="signin.php">
+            <button type="button" class="text-white bg-green-600 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-1.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 mr-0"> Logout
+            </button>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
